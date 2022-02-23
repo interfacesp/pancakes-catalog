b099a5e1718a042f14c56d558b03513806b1e3b0
Espoir <espoir.gahungere@interface3.be>
2022-02-23 17:40:20 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pancakes-catalog',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

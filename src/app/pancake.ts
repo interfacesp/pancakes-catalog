a2dcf0a3b9974933c7db521e86513ff7f5bdc5b5
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-03-02 16:16:16 +0100

    Add user photo to Pancake interface

--- src/app/pancake.ts
+++ src/app/pancake.ts
@@ -2,34 +2,44 @@ export interface Pancake {
     id: number; 
     name: string, 
     description: string,
-    location: {
-        longi: number, 
-        lati: number
-    }
+    location?: MyLocation,
+    picture?: UserPhoto
 
 }
 
+export interface UserPhoto {
+    filePath?: string ,
+    webViewPath?: string
+}
+
+export interface MyLocation {
+    longi?: number, 
+    lati?: number
+}
 
 export const pancakesCollection = [
     {
         id: 1,
         name: "crêpe sarrasin", 
         description: "une délicieuse crêpe faite à la main",
-        location : { longi: 120, lati: 50}
+        location : { longi: 120, lati: 50},
+        picture : {}
     },
 
     {
         id: 2,
         name: "crêpe chocolat", 
         description: "Du chocolat",
-        location: { longi: 120, lati: 50}
+        location: {},
+        picture : {}
     },
    
     {
         id: 3,
         name: "crêpe sucre", 
         description: "Du sucre",
-        location: { longi: 120, lati: 50}
+        location: { longi: 120, lati: 50},
+        picture : {}
     },
    
     
